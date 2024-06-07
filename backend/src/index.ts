001356bf462e688c04870a3e626c4725fbf18854
Zoran Simonovic <simonovic1980@gmail.com>
2024-06-07 19:16:25 +0200

    creating user

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
-// import userRoutes from "./routes/users";
+import userRoutes from "./routes/users";
 // import authRoutes from "./routes/auth";
 // import cookieParser from "cookie-parser";
 // import path from "path";
@@ -31,14 +31,14 @@ app.use(cors());
 // app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
 // app.use("/api/auth", authRoutes);
-// app.use("/api/users", userRoutes);
+app.use("/api/users", userRoutes);
 // app.use("/api/my-hotels", myHotelRoutes);
 // app.use("/api/hotels", hotelRoutes);
 // app.use("/api/my-bookings", bookingRoutes);
 
-app.get("/api/test", async (req: Request, res: Response) => {
-  res.json({message: 'hello from express'})
-});
+// app.get("/api/test", async (req: Request, res: Response) => {
+//   res.json({message: 'hello from express'})
+// });
 // app.get("*", (req: Request, res: Response) => {
 //   res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
 // });
