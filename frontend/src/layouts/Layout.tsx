2a96cc8b46c19f61e240f0a0f24c0aec13639437
Zoran Simonovic <simonovic1980@gmail.com>
2024-06-07 10:53:10 +0200

    routing app

--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -3,11 +3,11 @@ import Header from "../components/Header";
 import Hero from "../components/Hero";
 import SearchBar from "../components/SearchBar";
 
-interface Props {
-  children: React.ReactNode;
-}
-
-const Layout = ({ children }: Props) => {
+// interface Props {
+//   children: React.ReactNode;
+// }
+// { children }: Props)
+const Layout = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
@@ -15,7 +15,7 @@ const Layout = ({ children }: Props) => {
       <div className="container mx-auto">
         <SearchBar />
       </div>
-      <div className="container mx-auto py-10 flex-1">{children}</div>
+      {/* <div className="container mx-auto py-10 flex-1">{children}</div> */}
       <Footer />
     </div>
   );
